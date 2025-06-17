a4cff32202deb6b8485ea7e2a5c16251f462f398
Raed Salah Bzour <raedbzour77@gmail.com>
2025-06-17 19:10:29 +0300

    setting up the ngrx and creating action,reducer and use them inside the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { BookReducer } from './books/book.reducer';
+import { BookListComponent } from './book-list/book-list.component';
+import { AppState } from './app.state';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, BookListComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot<AppState>({ book: BookReducer }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
